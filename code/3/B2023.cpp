317aa8e94d203140dd0cd0fad8467f1e601f5fcf
Wing <Wing935@outlook.com>
2025-12-25 00:13:02 +0800

    没啥

--- code/3/B2023.cpp
+++ code/3/B2023.cpp
@@ -8,6 +8,6 @@ int main(){
   double ind;
 cin>>inc>>ini>>inf>>ind;
 //cout<<inc<<" "<<ini<<" "<<inf<<" "<<ind<<endl;
-//cout<<format("{:} {:} {:.{}f} {:.{}f}",inc,ini,inf,6,ind,6)<<endl;
-cout<<format("{0:} {1:} {2:.{3}f} {4:.6f}",inc,ini,inf,6,ind)<<endl;
+cout<<format("{:} {:} {:.{}f} {:.{}f}",inc,ini,inf,6,ind,6)<<endl;
+//cout<<format("{0:} {1:} {2:.{3}f} {4:.6f}",inc,ini,inf,6,ind)<<endl;
 }
