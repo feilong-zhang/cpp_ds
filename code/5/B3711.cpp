7048363b98e041d6b0130b98d9651019a7bf92fd
Wing <Wing935@outlook.com>
2026-01-05 23:50:26 +0800

    ac B3711，vector中v.erase(first, last);删除的是 [first, last)，即“包含 first，不包含 last”。

--- code/5/B3711.cpp
+++ code/5/B3711.cpp
@@ -1,16 +1,49 @@
 #include<iostream>
 #include<vector>
+#include<cstring>
 using namespace std;
-
+bool yes(vector<char> bak){
+  if(bak.size()<2) return (bak.back()-'0')%4==0;
+  int sum=0;
+  sum=sum+bak.back()-'0';
+  bak.pop_back();
+  sum=sum+(bak.back()-'0')*10;
+  return sum%4==0;
+}
 int main(){
   int T;
   cin>>T;
-  vector<char> v;
-  for(char in;cin>>in&&in!='\n';) {
-    v.push_back(in);
+  for(int k=0;k<T;k++){
+    char s[25];
+    cin >> s;
+    int n = (int)strlen(s);
+    //cout<<"n="<<n<<endl;
+    vector<char> v;
+    for(int i=0;i<n;i++){
+      v.push_back(s[i]);
+    }
+    int t=0;
+    if(yes(v)){
+      cout<<"Yes"<<endl;t=1;continue;
+    }
+    for(int k=1;k<n&&t==0;k++){
+      for(int j=0;j<2;j++){
+        vector<char> bak=v;
+        // for(int i=0;i<bak.size();i++){
+        //   cout<<bak.at(i);
+        // }cout<<endl;
+        bak.erase(bak.begin()+n-k-j,bak.end()-j);//cout<<"erase:"<<endl;
+        // for(int i=0;i<bak.size();i++){
+        //   cout<<bak.at(i);
+        // }cout<<endl;
+          if(yes(bak)){
+            cout<<"Yes"<<endl;t=1;break;
+          }
+      }
+    }
+        
+    if(t==0) cout<<"No"<<endl;
   }
-  for(;;){
-    
-  }
+  
 
 }
